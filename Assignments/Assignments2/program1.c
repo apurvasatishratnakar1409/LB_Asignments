40a37673d1ed14a6e76bfe113441403352199af0
Apurva Satish Ratnakar <apurvasatishratnakar1409@gmail.com>
2025-11-04 01:30:02 +0530

    program1.c
    The program takes a number from the user.
    
    Then it prints that many * (stars) on the screen.
    
    If the user enters a negative number, the program converts it to positive.
    
    A while loop is used for printing stars one by one until the count reaches the given number.

---
+++ Assignments/Assignments2/program1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo<0)
+    {
+        iNo = -iNo;
+    }
+
+
+    while(iCnt < iNo)
+    {
+        printf("* ");
+        iCnt++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number from user\n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+
+    return 0;
+}
