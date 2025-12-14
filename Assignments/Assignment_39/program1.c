d414f5635ba83c70e80d16224ef0badcdaeb2a95
Apurva Satish Ratnakar <apurvasatishratnakar1409@gmail.com>
2025-12-14 11:10:42 +0530

    Add recursive display function in program1.c

---
+++ Assignments/Assignment_39/program1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void Display(int NO)
+{
+    
+   if(NO==0)
+   {
+        return;
+   }
+    printf("*\t");
+    Display(NO-1);  
+}
+
+int main()
+{
+    Display(5);
+    return 0;
+}
